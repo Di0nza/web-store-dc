e870806d99904ee32b09a812fea2a23fe7f1b86f
maticht <maticht.1999@gmail.com>
2024-03-03 18:49:18 +0100

    article sort media and style

--- src/primitives/Button/Button.tsx
+++ src/primitives/Button/Button.tsx
@@ -9,53 +9,55 @@ interface Props {
 }
 
 export const Button = forwardRef<
-  HTMLButtonElement,
-  ComponentProps<"button"> & Props
->(({ variant = "primary", icon, children, className, ...props }, ref) => (
-  <button
-    ref={ref}
-    className={clsx(
-      className,
-      styles.button,
-      icon && !children && styles.iconButton,
-      {
-        [styles.buttonPrimary]: variant === "primary",
-        [styles.buttonSecondary]: variant === "secondary",
-        [styles.buttonSubtle]: variant === "subtle",
-        [styles.buttonDestructive]: variant === "destructive",
-      }
-    )}
-    {...props}
-  >
-    {icon && <span className={styles.icon}>{icon}</span>}
-    {children && <span className={styles.label}>{children}</span>}
-  </button>
-));
+    HTMLButtonElement,
+    ComponentProps<"button"> & Props
+>(function Button({ variant = "primary", icon, children, className, ...props }, ref) {
+    return (
+        <button
+            ref={ref}
+            className={clsx(
+                className,
+                styles.button,
+                icon && !children && styles.iconButton,
+                {
+                    [styles.buttonPrimary]: variant === "primary",
+                    [styles.buttonSecondary]: variant === "secondary",
+                    [styles.buttonSubtle]: variant === "subtle",
+                    [styles.buttonDestructive]: variant === "destructive",
+                }
+            )}
+            {...props}
+        >
+            {icon && <span className={styles.icon}>{icon}</span>}
+            {children && <span className={styles.label}>{children}</span>}
+        </button>
+    );
+});
 
 export function LinkButton({
-  variant = "primary",
-  icon,
-  children,
-  className,
-  ...props
-}: ComponentProps<typeof Link> & Props) {
-  return (
-    <Link
-      className={clsx(
-        className,
-        styles.button,
-        icon && !children && styles.iconButton,
-        {
-          [styles.buttonPrimary]: variant === "primary",
-          [styles.buttonSecondary]: variant === "secondary",
-          [styles.buttonSubtle]: variant === "subtle",
-          [styles.buttonDestructive]: variant === "destructive",
-        }
-      )}
-      {...props}
-    >
-      {icon && <span className={styles.icon}>{icon}</span>}
-      {children && <span className={styles.label}>{children}</span>}
-    </Link>
-  );
+                               variant = "primary",
+                               icon,
+                               children,
+                               className,
+                               ...props
+                           }: ComponentProps<typeof Link> & Props) {
+    return (
+        <Link
+            className={clsx(
+                className,
+                styles.button,
+                icon && !children && styles.iconButton,
+                {
+                    [styles.buttonPrimary]: variant === "primary",
+                    [styles.buttonSecondary]: variant === "secondary",
+                    [styles.buttonSubtle]: variant === "subtle",
+                    [styles.buttonDestructive]: variant === "destructive",
+                }
+            )}
+            {...props}
+        >
+            {icon && <span className={styles.icon}>{icon}</span>}
+            {children && <span className={styles.label}>{children}</span>}
+        </Link>
+    );
 }
