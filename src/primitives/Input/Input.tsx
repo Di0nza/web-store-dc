0c9ee8c3a2d46e64276f340418228ad369206ab3
maticht <maticht.1999@gmail.com>
2024-03-03 19:00:46 +0100

    fix Error:missing display name

--- src/primitives/Input/Input.tsx
+++ src/primitives/Input/Input.tsx
@@ -3,9 +3,10 @@ import { ComponentProps, forwardRef } from "react";
 import styles from "./Input.module.css";
 
 export const Input = forwardRef<HTMLInputElement, ComponentProps<"input">>(
-  ({ className, ...props }, ref) => {
-    return (
-      <input ref={ref} className={clsx(className, styles.input)} {...props} />
-    );
-  }
+    function Input({ className, ...props }, ref) {
+        return (
+            <input ref={ref} className={clsx(className, styles.input)} {...props} />
+        );
+    }
 );
+
