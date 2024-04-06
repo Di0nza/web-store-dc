52bc1d306e9b8e09aaad7e0e2c59f96232497f13
maticht <maticht.1999@gmail.com>
2024-04-06 19:22:00 +0200

    advertising front

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,17 @@ import OwnDesignOrder from "@/components/homeScreen/ownDesignOrder";
 import {NewCollection} from "@/components/homeScreen/newСollection";
 import {BestProducts} from "@/components/homeScreen/BestProducts";
 import SplashScreen from "@/components/SplashScreen/SplashScreen";
+import type {Metadata} from "next";
+
+export const metadata: Metadata = {
+    title: 'MariDeniz',
+    description: 'Shop unique fashion clothing with signature designs',
+    keywords: ['Clothing', 'fashion', 'women\'s clothing', 'men\'s clothing'],
+    openGraph: {
+        images: 'https://res.cloudinary.com/maticht12345/image/upload/v1701277508/Letter_-_16_wds2cz.png',
+    },
+    icons: 'https://res.cloudinary.com/maticht12345/image/upload/v1701277508/Letter_-_16_wds2cz.png',
+}
 
 export default function Home() {
     return (
