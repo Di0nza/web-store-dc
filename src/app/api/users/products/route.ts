43f5e2c188f65d75fb0a1f9cc337a4aa7e6b3f53
marideniz <mari28deniz@gmail.com>
2024-05-29 20:09:43 +0200

    Update get products
    Update get products

--- src/app/api/users/products/route.ts
+++ src/app/api/users/products/route.ts
@@ -4,15 +4,23 @@ import {NextRequest, NextResponse} from "next/server";
 import {IProduct} from "@/types/Product";
 import {currentUser, isAdmin} from "@/lib/auth";
 
-export async function GET(){
-    try{
-        const products:IProduct[] = await Product.find();
-        return NextResponse.json({
+export async function GET(req: NextRequest) {
+    try {
+        const products: IProduct[] = await Product.find();
+        const headers = new Headers();
+        headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+        headers.set('Pragma', 'no-cache');
+        headers.set('Expires', '0');
+
+        return new NextResponse(JSON.stringify({
             message: "All products",
             success: true,
             products
-        })
+        }), {
+            status: 200,
+            headers: headers
+        });
     } catch (error: any) {
-        return NextResponse.json({error: error.message}, {status: 500})
+        return NextResponse.json({ error: error.message }, { status: 500 });
     }
 }
