83b981f5af7064e51b603f2dc75b713ad662d01e
Dmitry Pavlovich <dimonza49@gmail.com>
2024-06-11 16:51:26 +0300

    user modal fix

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema<IUser>({
     name: {
         type: String,
         required: [true, "Please provide a name"],
-        unique: true,
+        unique: false,
     },
     email: {
         type: String,
